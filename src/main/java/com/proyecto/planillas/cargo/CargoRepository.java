14c2c232101fb7f55a25d88d8c4c6d5ba205eb2b
Snowru <melds3101@gmail.com>
2025-11-02 11:40:55 -0500

    Feature CargoRepository completo

---
+++ src/main/java/com/proyecto/planillas/cargo/CargoRepository.java
@@ -0,0 +1,7 @@
+package com.proyecto.planillas.cargo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CargoRepository extends JpaRepository<Cargo, Long> {
+    
+}
