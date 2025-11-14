5af2a851e213e55125964f81f70c03ebc383fd77
Snowru <melds3101@gmail.com>
2025-11-13 20:09:49 -0500

    Feature Mapper and Repository completos

--- src/main/java/com/proyecto/planillas/usuario/UsuarioMapper.java
+++ src/main/java/com/proyecto/planillas/usuario/UsuarioMapper.java
@@ -1,5 +1,28 @@
 package com.proyecto.planillas.usuario;
 
-public class UsuarioMapper {
+import org.springframework.stereotype.Component;
+
+import com.proyecto.planillas.common.MapperInterface;
+
+@Component
+public class UsuarioMapper implements MapperInterface <Usuario, UsuarioWrite, UsuarioView>{
+
+    @Override
+    public UsuarioView toDto(Usuario entity) {
+        return new UsuarioView(
+            entity.getId(),
+            entity.getUser(),
+            entity.getPassword()
+        );
+    }
+
+    @Override
+    public Usuario toEntity(UsuarioWrite dto) {
+        return Usuario.builder()
+        .id(dto.id())
+        .user(dto.user())
+        .password(dto.password())
+        .build();
+    }
     
 }
--- src/main/java/com/proyecto/planillas/usuario/UsuarioRepository.java
+++ src/main/java/com/proyecto/planillas/usuario/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.proyecto.planillas.usuario;
 
-public class UsuarioRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository <Usuario, Long>{
 }
