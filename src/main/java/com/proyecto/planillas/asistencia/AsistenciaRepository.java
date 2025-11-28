225a41358a6bc843cda6babf40351d6770324cd8
Frost173 <jordantrejo45@gmail.com>
2025-11-28 02:57:31 -0500

    Ultimos cambios en asistencia

--- src/main/java/com/proyecto/planillas/asistencia/AsistenciaRepository.java
+++ src/main/java/com/proyecto/planillas/asistencia/AsistenciaRepository.java
@@ -1,7 +1,7 @@
 package com.proyecto.planillas.asistencia;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AsistenciaRepository extends JpaRepository<Asistencia, Long>{
+public interface AsistenciaRepository{
 
+    
 }
